e4a2458bd2a5593caff86bfd6a26b30c90fbb49d
Paulo Melo <peimelo@gmail.com>
2020-11-17 21:18:20 -0300

    Exibe item ativo em destaque

--- src/app/core/components/nav-item.component.ts
+++ src/app/core/components/nav-item.component.ts
@@ -3,7 +3,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-nav-item',
   template: `
-    <a mat-list-item [routerLink]="routerLink" (click)="navigate.emit()">
+    <a
+      mat-list-item
+      [routerLink]="routerLink"
+      routerLinkActive="active-list-item"
+      (click)="navigate.emit()"
+    >
       <fa-icon [icon]="['fas', icon]" mat-list-icon></fa-icon>
       <span mat-line><ng-content></ng-content></span>
       <span mat-line class="secondary">{{ hint }}</span>
@@ -15,12 +20,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
       .secondary {
         color: rgba(0, 0, 0, 0.54);
       }
+
+      .active-list-item {
+        color: #3f51b5 !important; /* Note: You could also use a custom theme */
+      }
     `,
   ],
 })
 export class NavItemComponent {
   @Input() hint = '';
   @Input() icon = '';
-  @Input() routerLink: string | any[] = '/';
+  @Input() routerLink: string;
   @Output() navigate = new EventEmitter();
 }
