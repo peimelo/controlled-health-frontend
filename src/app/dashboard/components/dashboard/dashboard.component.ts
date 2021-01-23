f78bea91733c4555d43cba1e717f0d6039e69599
Paulo Melo <peimelo@gmail.com>
2021-01-23 17:53:05 -0300

    Retira timeline do chart

--- src/app/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -11,7 +11,6 @@ export class DashboardComponent {
   colorScheme = {
     domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5'],
   };
-  timeline = true;
 
   xAxis = true;
   xAxisLabel = 'Date';
