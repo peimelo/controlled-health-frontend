b1eb1e2c34c544c18d31312c65e520856b3220c7
Paulo Melo <peimelo@gmail.com>
2021-03-11 08:39:45 -0300

    Updates effects

--- src/app/dashboard/actions/dashboard-api.actions.ts
+++ src/app/dashboard/actions/dashboard-api.actions.ts
@@ -1,11 +1,6 @@
 import { createAction, props } from '@ngrx/store';
 import { Dashboard } from '../models';
 
-export const loadDashboardFailure = createAction(
-  '[Dashboard/API] Load Dashboard Failure',
-  props<{ error: any }>()
-);
-
 export const loadDashboardSuccess = createAction(
   '[Dashboard/API] Load Dashboard Success',
   props<{ dashboard: Dashboard }>()
--- src/app/dashboard/effects/dashboard.effects.ts
+++ src/app/dashboard/effects/dashboard.effects.ts
@@ -1,29 +1,29 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect, ofType } from '@ngrx/effects';
-import { of } from 'rxjs';
-import { catchError, map, switchMap } from 'rxjs/operators';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, exhaustMap, map } from 'rxjs/operators';
+import { ErrorsService } from '../../shared/services/errors.service';
 import { DashboardApiActions, DashboardPageActions } from '../actions';
 import { DashboardService } from '../services/dashboard.service';
 
 @Injectable()
 export class DashboardEffects {
-  constructor(
-    private actions$: Actions,
-    private dashboardService: DashboardService
-  ) {}
-
-  @Effect()
-  loadDashboard$ = this.actions$.pipe(
-    ofType(DashboardPageActions.loadDashboard),
-    switchMap(() =>
-      this.dashboardService.getAll().pipe(
-        map((dashboard) =>
-          DashboardApiActions.loadDashboardSuccess({ dashboard })
-        ),
-        catchError((error) => {
-          return of(DashboardApiActions.loadDashboardFailure({ error }));
-        })
+  loadDashboard$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(DashboardPageActions.loadDashboard),
+      exhaustMap(() =>
+        this.dashboardService.getAll().pipe(
+          map((dashboard) =>
+            DashboardApiActions.loadDashboardSuccess({ dashboard })
+          ),
+          catchError((error) => this.errorService.showError(error))
+        )
       )
     )
   );
+
+  constructor(
+    private actions$: Actions,
+    private dashboardService: DashboardService,
+    private errorService: ErrorsService
+  ) {}
 }
